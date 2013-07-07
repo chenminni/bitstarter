3cfbc78fa973c40500d049ff33878ea7318b2906
Chen Min Ni <chen.min.ni@intel.com>
2013-07-07 08:49:40 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fileBuf = fs.readFileSync (index.html)
+  //response.send('Hello World 2!');
+  response.send(buf.toString(fileBuf));
 });
 
 var port = process.env.PORT || 5000;
