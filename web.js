b7f7fd38b463f1051dce6743bdc16efc6c1211c3
Chen Min Ni <chen.min.ni@intel.com>
2013-07-07 09:18:01 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fileBuf = fs.readFileSync (index.html)
+  var fileBuf = fs.readFileSync ("index.html")
   //response.send('Hello World 2!');
   response.send(buf.toString(fileBuf));
 });
